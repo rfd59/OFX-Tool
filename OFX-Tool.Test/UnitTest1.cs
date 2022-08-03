f540a523ce23eb3db7e85e8979870a5f21dcec72
Richard FAVREAU <rfavreau@rfd.ovh>
2022-08-03 10:14:18 +0200

    Add First Test

--- OFX-Tool.Test/UnitTest1.cs
+++ OFX-Tool.Test/UnitTest1.cs
@@ -1,13 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFD.OFXTool.Library;
+using System.IO;
 
-namespace OFX_Tool.Test
+namespace RFD.OFXTool.Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
         public void TestMethod1()
         {
+            OFXTool.Library.OFXTool.GetExtract("test.file");
         }
     }
 }
