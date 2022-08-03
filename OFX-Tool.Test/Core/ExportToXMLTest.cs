732bdb93546cd6d30ce578096812392015401164
Richard FAVREAU <rfavreau@rfd.ovh>
2022-08-03 11:38:45 +0200

    Add ExportToXML Test

---
+++ OFX-Tool.Test/Core/ExportToXMLTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFD.OFXTool.Library.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFD.OFXTool.Test.Core
+{
+    [TestClass]
+    public class ExportToXMLTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void FileNotFound()
+        {
+            new ExportToXML("FileNotFound.ofx");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidXmlName()
+        {
+            new ExportToXML("UnitTest1.ofx", "InvalidXmlName.txt");
+        }
+    }
+}
