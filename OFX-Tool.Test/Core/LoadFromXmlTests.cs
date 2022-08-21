1f7f7df375722624545c748437a38fc987be1fb9
Richard FAVREAU <rfavreau@rfd.ovh>
2022-08-21 18:23:52 +0200

    Clean code

--- OFX-Tool.Test/Core/LoadFromXmlTests.cs
+++ OFX-Tool.Test/Core/LoadFromXmlTests.cs
@@ -24,7 +24,7 @@ namespace RFD.OFXTool.Library.Core.Tests
             var bankacctfrom = new BankAccount() { BankId = "12345", BranchId = "6789", AccountId = "00112233445", AccountType = AccountEnum.CHECKING };
             var stmttrn = new List<StatementTransaction>();
             stmttrn.Add(new StatementTransaction() { TransactionType = TransactionEnum.CREDIT, PostedDate = "20220602", UserDate = "20220602", TransactionAmount = "450.00", FinancialInstitutionTransactionId = "AZ1ERT23YU", Memo = "VIR SURNAME NAME" });
-            stmttrn.Add(new StatementTransaction() { TransactionType = TransactionEnum.DEBIT, PostedDate = "20220702", UserDate = "20220702", TransactionAmount = "50.00", FinancialInstitutionTransactionId = "NBVCXW98", Memo = "FOOD", CheckNumber= "123456" });
+            stmttrn.Add(new StatementTransaction() { TransactionType = TransactionEnum.DEBIT, PostedDate = "20220702", UserDate = "20220702", TransactionAmount = "50.00", FinancialInstitutionTransactionId = "NBVCXW98", Memo = "FOOD", CheckNumber = "123456" });
             var banktranslist = new BankTransactionList() { StartDate = "20220602000000", EndDate = "20220728000000", StatementTransactions = stmttrn };
             var ledgerbal = new LedgerBalance() { BalanceAmount = "12345.67", DateAsOf = "20220728000000" };
             var availbal = new AvailableBalance() { BalanceAmount = "0.00", DateAsOf = "20220728000000" };
--- OFX-Tool.Test/OFXToolTests.cs
+++ OFX-Tool.Test/OFXToolTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RFD.OFXTool.Library;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RFD.OFXTool.Library.Tests
 {
