30a3be2f619afd7fd0ea61c6416fc3f6243cd6e3
Richard FAVREAU <rfavreau@rfd.ovh>
2022-08-21 18:22:39 +0200

    Add OFXTool.GetExtract Unit Tests

---
+++ OFX-Tool.Test/OFXToolTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFD.OFXTool.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFD.OFXTool.Library.Tests
+{
+    [TestClass()]
+    public class OFXToolTests
+    {
+        [TestMethod()]
+        public void GetExtractTest()
+        {
+            var document = OFXTool.GetExtract("TestFiles/ExportToXmlTest1.ofx");
+            Assert.IsNotNull(document);
+            Assert.AreEqual("20220802000000", document.SignonResponseMessageSetV1.SignonResponse.ServerDate);
+            Assert.AreEqual("450.00", document.BankResponseMessageSetV1.StatementTransactionResponses[0].StatementResponse.BankTransactionList.StatementTransactions[0].TransactionAmount);
+        }
+
+    }
+}
