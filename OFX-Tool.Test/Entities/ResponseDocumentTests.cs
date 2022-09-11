a7d2e6d748e01070050c08ad1b8a4381f8c378a2
Richard FAVREAU <rfavreau@rfd.ovh>
2022-09-11 15:41:16 +0200

    Add ResponseDocument tests

---
+++ OFX-Tool.Test/Entities/ResponseDocumentTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RFD.OFXTool.Library.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFD.OFXTool.Library.Entities.Tests
+{
+    [TestClass()]
+    public class ResponseDocumentTests
+    {
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            var entity = new ResponseDocument();
+            Assert.IsFalse(entity.Equals(null));
+            Assert.IsFalse(entity.Equals(new Object()));
+            Assert.AreEqual(entity, new ResponseDocument());
+            Assert.AreNotEqual(entity, new ResponseDocument() { BankResponseMessageSetV1 = new BankResponseMessageSetV1() });
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            var entity = new ResponseDocument();
+            Assert.IsNotNull(entity.GetHashCode());
+        }
+    }
+}
