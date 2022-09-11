fb1ea075ae8fafe65e241435255518ce208c3ba2
Richard FAVREAU <rfavreau@rfd.ovh>
2022-09-11 20:21:02 +0200

    Add abstract element test

---
+++ OFX-Tool.Test/Core/Elements/ElementAbstractTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml;
+
+namespace RFD.OFXTool.Library.Core.Elements.Tests
+{
+
+    [TestClass()]
+    public class ElementAbstractTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void LoadTest_NullReference()
+        {
+            new TestElement().Load(null);
+        }
+
+    }
+
+    internal class TestElement : ElementAbstract<Object>
+    {
+        protected override void BuildElement(object doc)
+        {
+        }
+
+        protected override void LoadElement(XmlTextReader xmlReader)
+        {
+        }
+    }
+}
