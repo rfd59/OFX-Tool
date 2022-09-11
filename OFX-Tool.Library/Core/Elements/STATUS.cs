fbf0c5ffe3c001bad882bede2f9b508c4f2fc5df
Richard FAVREAU <rfavreau@rfd.ovh>
2022-09-11 13:16:21 +0200

    Fix STATUS element

--- OFX-Tool.Library/Core/Elements/STATUS.cs
+++ OFX-Tool.Library/Core/Elements/STATUS.cs
@@ -10,6 +10,8 @@ namespace RFD.OFXTool.Library.Core.Elements
         {
             if (doc.Code != null)
                 _build.AppendLine(WriteProperty<Status>(nameof(Status.Code), doc.Code, _level));
+            if (doc.Message != null)
+                _build.AppendLine(WriteProperty<Status>(nameof(Status.Message), doc.Message, _level));
             if (doc.Severity != null)
                 _build.AppendLine(WriteProperty<Status>(nameof(Status.Severity), doc.Severity, _level));
         }
@@ -20,6 +22,8 @@ namespace RFD.OFXTool.Library.Core.Elements
             {
                 if (_field == Entity.GetElement<Status>(nameof(Status.Code)))
                     _load.Code = xmlReader.Value;
+                else if (_field == Entity.GetElement<Status>(nameof(Status.Message)))
+                    _load.Message = xmlReader.Value;
                 else if (_field == Entity.GetElement<Status>(nameof(Status.Severity)))
                     _load.Severity = (SeverityEnum)Enum.Parse(typeof(SeverityEnum), xmlReader.Value);
                 else
